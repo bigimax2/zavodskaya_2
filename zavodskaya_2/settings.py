3af60becb3f678e629d696a11771ba06306ada92
bigimax2 <mark25@inbox.ru>
2022-12-06 10:33:54 +0300

    1 с настройкой статики

--- zavodskaya_2/settings.py
+++ zavodskaya_2/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nc^6^!rr4vhyy@s%$$)&ps_eclj6%@_wpi-r0ld*35r=%yt%lv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -112,8 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'zavodskaya_2/static')
-STATICFILES_DIRS = ['']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- zavodskaya_2/urls.py
+++ zavodskaya_2/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls import url
 from django.contrib import admin
 
 from django.urls import path, include
 from django.conf.urls.static import static
 from zavodskaya_2 import settings
+from django.views.static import serve as mediaserve
 
 
 urlpatterns = [
@@ -27,6 +28,21 @@ urlpatterns = [
 
 ]
 
-
+# Доп настройка для отображения статики для админки и сайта при разворачивании на хостинге при отсутствии доступа
+# к настройке NGINX.
 if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+else:
+    urlpatterns += [
+        url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.MEDIA_ROOT}),
+        url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.STATIC_ROOT}),
+    ]
