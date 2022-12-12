5f29b2999ac796db3756ea614f6aa33e0e28f0fb
bigimax2 <mark25@inbox.ru>
2022-12-12 23:58:32 +0300

    не законченные модели

--- service_2/models.py
+++ service_2/models.py
@@ -1,17 +1,73 @@
 from django.db import models
 
 
-class Orders(models.Model):
-    brand = models.CharField(max_length=10)
-    model = models.CharField(max_length=10)
-    reg_num = models.CharField(max_length=8)
-    owner = models.CharField(max_length=255)
-    color = models.CharField(max_length=10)
-    vin = models.CharField(max_length=18)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+class ModelCar(models.Model):
+    id_model = models.AutoField(primary_key=True, null=True)
+    model = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.model
+
+
+class BrandCar(models.Model):
+    id_brand = models.AutoField(primary_key=True, null=True)
+    brand = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.brand
+
+
+class Status(models.Model):
+    STATUS = [
+        ('active', 'active'),
+        ('inactive', 'inactive'),
+    ]
+    id_status = models.AutoField(primary_key=True, null=True)
+    status = models.CharField(choices=STATUS, max_length=20, default='active', verbose_name='Статус заказа')
+
+    def __str__(self):
+        return self.status
+
+
+class OwnerCar(models.Model):
+    id_owner = models.AutoField(primary_key=True, null=True)
+    firstname_owner = models.CharField(max_length=20, null=True)
+    lastname_owner = models.CharField(max_length=20, null=True, blank=True)
+    phone_owner = models.CharField(max_length=11, null=True)
+    email_owner = models.EmailField(max_length=20, null=True, blank=True)
+    comments_owner = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.firstname_owner
+
+
+class VinCar(models.Model):
+    id_vin = models.AutoField(primary_key=True, null=True)
+    vin = models.CharField(max_length=18, null=True, blank=True)
+
+    def __str__(self):
+        return self.vin
+
+
+class RegNumCar(models.Model):
+    id_reg = models.AutoField(primaryKey=True, null=True, blank=True)
+    reg_num = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.reg_num
+
+
+class Orders(models.Model):
+    id_order = models.AutoField(primary_key=True, null=True)
+    color = models.CharField(max_length=10)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    model_car = models.ForeignKey(ModelCar, null=True, blank=True, on_delete=models.CASCADE)
+    brand_car = models.ForeignKey(BrandCar, null=True, blank=True, on_delete=models.CASCADE)
+    status_order = models.ForeignKey(Status, null=True, blank=True, on_delete=models.CASCADE)
+    owner_car = models.ForeignKey(OwnerCar, null=True, blank=True, on_delete=models.CASCADE)
+    vin_car = models.ForeignKey(VinCar, null=True, blank=True, on_delete=models.CASCADE)
+    regnum_car = models.ForeignKey(RegNumCar, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id_order
