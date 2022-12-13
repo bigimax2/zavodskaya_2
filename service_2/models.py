ef99a313baad3dfaf388bd197b03049b7c9e3254
bigimax2 <mark25@inbox.ru>
2022-12-14 00:20:45 +0300

    законченные модели

--- service_2/models.py
+++ service_2/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ModelCar(models.Model):
-    id_model = models.AutoField(primary_key=True, null=True)
+    id_model = models.AutoField(primary_key=True)
     model = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
@@ -10,7 +10,7 @@ class ModelCar(models.Model):
 
 
 class BrandCar(models.Model):
-    id_brand = models.AutoField(primary_key=True, null=True)
+    id_brand = models.AutoField(primary_key=True)
     brand = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
@@ -22,7 +22,7 @@ class Status(models.Model):
         ('active', 'active'),
         ('inactive', 'inactive'),
     ]
-    id_status = models.AutoField(primary_key=True, null=True)
+    id_status = models.AutoField(primary_key=True)
     status = models.CharField(choices=STATUS, max_length=20, default='active', verbose_name='Статус заказа')
 
     def __str__(self):
@@ -30,7 +30,7 @@ class Status(models.Model):
 
 
 class OwnerCar(models.Model):
-    id_owner = models.AutoField(primary_key=True, null=True)
+    id_owner = models.AutoField(primary_key=True)
     firstname_owner = models.CharField(max_length=20, null=True)
     lastname_owner = models.CharField(max_length=20, null=True, blank=True)
     phone_owner = models.CharField(max_length=11, null=True)
@@ -42,7 +42,7 @@ class OwnerCar(models.Model):
 
 
 class VinCar(models.Model):
-    id_vin = models.AutoField(primary_key=True, null=True)
+    id_vin = models.AutoField(primary_key=True)
     vin = models.CharField(max_length=18, null=True, blank=True)
 
     def __str__(self):
@@ -50,7 +50,7 @@ class VinCar(models.Model):
 
 
 class RegNumCar(models.Model):
-    id_reg = models.AutoField(primaryKey=True, null=True, blank=True)
+    id_reg = models.AutoField(primary_key=True, blank=True)
     reg_num = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
@@ -58,7 +58,7 @@ class RegNumCar(models.Model):
 
 
 class Orders(models.Model):
-    id_order = models.AutoField(primary_key=True, null=True)
+    id_order = models.AutoField(primary_key=True)
     color = models.CharField(max_length=10)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
@@ -68,6 +68,31 @@ class Orders(models.Model):
     owner_car = models.ForeignKey(OwnerCar, null=True, blank=True, on_delete=models.CASCADE)
     vin_car = models.ForeignKey(VinCar, null=True, blank=True, on_delete=models.CASCADE)
     regnum_car = models.ForeignKey(RegNumCar, null=True, blank=True, on_delete=models.CASCADE)
+    order_comments = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.id_order
+
+
+class Jobs(models.Model):
+    id_jobs = models.AutoField(primary_key=True, blank=True)
+    order_jobs = models.ManyToManyField(Orders, blank=True)
+    jobs_comments = models.CharField(max_length=255, null=True, blank=True)
+    jobs_price = models.IntegerField(null=True, blank=True)
+    file = models.FileField(blank=True, null=True, upload_to='media/file')
+    image = models.ImageField(blank=True, null=True, upload_to='media/image')
+    date_job = models.DateField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.jobs_comments
+
+
+class Consumers(models.Model):
+    id_consumers = models.AutoField(primary_key=True, blank=True)
+    jobs_consumers = models.ManyToManyField(Jobs, blank=True)
+    consumers_name = models.CharField(max_length=20, null=True, blank=True)
+    consumers_price = models.IntegerField(null=True, blank=True)
+    consumers_quantity = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.consumers_name
