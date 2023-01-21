089b0ff23ac5b3dd5b96faf3d200f00d9372a55d
bigimax2 <mark25@inbox.ru>
2023-01-21 22:41:32 +0300

    1.10.2 рабочая версия

--- service_2/utils.py
+++ service_2/utils.py
@@ -6,30 +6,27 @@ from service_2.models import Jobs, Consumers, Orders
 def cost_job(id_order):
     job = Jobs.objects.filter(order_id=id_order).values_list('jobs_price', flat=True)
     b = []
-    x = 0
     for j in job:
         b.append(j)
-    x += sum(b)
+    x = sum(b)
     return x
 
 
 def cost_cons(id_jobs):
     consum = Consumers.objects.filter(job_id=id_jobs).values_list('consumers_price', flat=True)
     b = []
-    x = 0
     for j in consum:
         b.append(j)
-    x += sum(b)
+    x = sum(b)
     return x
 
 
 def cost_cons_in_order(id_order):
     cust = Consumers.objects.filter(order_id=id_order).values_list('consumers_price', flat=True)
     c = []
-    y = 0
     for a in cust:
         c.append(a)
-    y += sum(c)
+    y = sum(c)
     return y
 
 
@@ -44,8 +41,7 @@ def all_cost(id_order):
 def prepayment(id_order):
     prepay = Orders.objects.filter(id_order=id_order).values_list('prepayment', flat=True)
     c = []
-    y = 0
     for a in prepay:
         c.append(a)
-    y += sum(c)
+    y = sum(c)
     return y
